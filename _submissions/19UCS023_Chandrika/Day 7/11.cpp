b5983fad0fbe1784126ac25406468416b3668e0d
Chandrika Acharjee <76817938+Chandrika372@users.noreply.github.com>
2021-06-29 15:14:37 +0530

    Merge k sorted lists

---
+++ _submissions/19UCS023_Chandrika/Day 7/11.cpp
@@ -0,0 +1,20 @@
+ListNode* Solution::mergeKLists(vector<ListNode*> &A) {
+ vector<int>v;
+ for(auto it:A)
+ {
+     while(it)
+     {
+        v.push_back(it->val);
+        it=it->next;
+     }
+ }
+ sort(v.begin(),v.end());
+ ListNode* temp=new ListNode(0);
+ ListNode* p=temp;
+ for(auto x:v)
+ {
+     p->next=new ListNode(x);
+     p=p->next;
+ }
+ return temp->next;
+}
